e7c0c2fc736f52cafb859f80c1d3970a39992b44
Daniel Penalva <dkajah@gmail.com>
2022-09-10 15:58:42 -0300

    correcting iam and s3 tf files

--- iac/iam.tf
+++ iac/iam.tf
@@ -3,7 +3,7 @@ resource "aws_iam_role" "lambda" {
 
     assume_role_policy = <<EOF
     {
-        "Version": "2012-10-17"
+        "Version": "2012-10-17",
         "Statement": [
             {
                 "Action": "sts:AssumeRole"
@@ -29,7 +29,7 @@ resource "aws_iam_policy" "lambda"{
 
     policy = <<EOF
     {
-        "Version": "2012-10-17"
+        "Version": "2012-10-17",
         "Statement": [
             {
                 "Effect": "Allow",
